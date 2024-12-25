0c74851ac00bfc4e5fccbc261b03d7d5fd1bbb3c
Vivek <vivekousik8555@gmail.com>
2024-12-25 20:58:13 +0530

    irrevocable

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,50 +1,5 @@
-import CredentialsProvider from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "@/app/libs/prismadb";
-import { SessionStrategy } from "next-auth";
-import bcrypt from "bcrypt";
 import NextAuth from "next-auth";
-
-export const authOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                email: { label: "Email", type: "text" },
-                password: { label: "Password", type: "password" },
-            },
-            async authorize(credentials) {
-                if (!credentials?.email || !credentials?.password) {
-                    throw new Error("Invalid credentials");
-                }
-
-                const user = await prisma.user.findUnique({
-                    where: { email: credentials.email },
-                });
-
-                if (!user || !user.hashedPassword) {
-                    throw new Error("Invalid credentials");
-                }
-
-                const isCorrectPassword = await bcrypt.compare(
-                    credentials.password,
-                    user.hashedPassword
-                );
-
-                if (!isCorrectPassword) {
-                    throw new Error("Invalid credentials");
-                }
-                return user;
-            },
-        }),
-    ],
-    debug: process.env.NODE_ENV === "development",
-    session: {
-        strategy: "jwt" as SessionStrategy,
-    },
-    secret: process.env.NEXTAUTH_SECRET,
-};
+import { authOptions } from "./authOptions";
 
 const handler = NextAuth(authOptions);
 
